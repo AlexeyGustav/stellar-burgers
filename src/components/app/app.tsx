96ca630feed8fe836b5a6e1b8541707d7e9003c3
Alexey Gustav <gustavaleksej9@gmail.com>
2025-04-09 19:08:11 +0300

    start

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,26 @@
 import { ConstructorPage } from '@pages';
+import { Feed } from '../../pages/feed/feed';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+    </Routes>
   </div>
 );
+// const App = () => (
+//   <div className={styles.app}>
+//     <AppHeader />
+//     <ConstructorPage />
+//   </div>
+// );
 
 export default App;
--- src/components/ui/feed-info/feed-info.tsx
+++ src/components/ui/feed-info/feed-info.tsx
@@ -8,7 +8,6 @@ export const FeedInfoUI: FC<FeedInfoUIProps> = memo(
   ({ feed, readyOrders, pendingOrders }) => {
     const { total, totalToday } = feed;
 
-
     return (
       <section>
         <div className={styles.columns}>
@@ -19,7 +18,7 @@ export const FeedInfoUI: FC<FeedInfoUIProps> = memo(
           />
           <HalfColumn orders={pendingOrders} title={'В работе'} />
         </div>
-        
+
         <Column title={'Выполнено за все время'} content={total} />
         <Column title={'Выполнено за сегодня'} content={totalToday} />
       </section>
