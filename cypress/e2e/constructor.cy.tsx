f466ce622fdc4f856d20a308f56df32d50928771
Alexey Gustav <gustavaleksej9@gmail.com>
2025-05-12 09:05:27 +0300

    fix consts

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,31 +1,39 @@
 ///<reference types="cypress" />
 
+const testUrl = 'http://localhost:4000';
+const souceIngridient = '643d69a5c3f7b9001cfa0942';
+const moveButton = 'move_button';
+const details = 'Детали ингредиента';
+const closeModal = 'close-modal';
+const orderNumber = 'order-number';
+const switching = 'switching';
+ 
 describe('проверяем доступность приложения', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:4000');
+    cy.visit(testUrl);
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as('getIngredients'); // Добавляем alias
   });
 
   it('обавление ингредиента в конструктор работает корректно', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0942]').find('button').click();
+    cy.get(`[data-cy=${souceIngridient}]`).find('button').click();
     cy.get('[data-cy=price]').should('have.text', '90');
-    cy.get('[data-cy=switching]')
-      .find('.move_button')
-      .should('have.class', 'move_button');
+    cy.get(`[data-cy=${switching}]`)
+      .find(`.${moveButton}`)
+      .should('have.class', `${moveButton}`);
   });
 
   it('открыть модальное окно', () => {
-    cy.contains('Детали ингредиента').should('not.exist');
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0942]').click();
-    cy.url().should('include', '/ingredients/643d69a5c3f7b9001cfa0942');
-    cy.contains('Детали ингредиента').should('exist');
+    cy.contains(details).should('not.exist');
+    cy.get(`[data-cy=${souceIngridient}]`).click();
+    cy.url().should('include', `/ingredients/${souceIngridient}`);
+    cy.contains(details).should('exist');
   })
 
   it('закрыть модальное окно', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0942]').click();
-    cy.contains('Детали ингредиента').should('exist');
-    cy.get('[data-cy=close-modal]').click();
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.get(`[data-cy=${souceIngridient}]`).click();
+    cy.contains(details).should('exist');
+    cy.get(`[data-cy=${closeModal}]`).click();
+    cy.contains(details).should('not.exist');
   })
 });
 
@@ -42,7 +50,7 @@ describe('cоздание заказа', () => {
       JSON.stringify('test-refreshToken')
     );
     cy.setCookie('accessToken', 'test-accessToken');
-    cy.visit('http://localhost:4000');
+    cy.visit(testUrl);
 
   });
 
@@ -65,17 +73,17 @@ describe('cоздание заказа', () => {
         cy.log('Ответ от сервера:', interception.response?.body);  // Выводим тело ответа в консоль
 
         // Проверяется, что модальное окно открылось и номер заказа верный
-        cy.get('[data-cy=order-number]').should('exist');
+        cy.get(`[data-cy=${orderNumber}]`).should('exist');
         cy.contains('76767').should('exist');
 
         // Закрывается модальное окно и проверяется успешность закрытия
-        cy.get('[data-cy=close-modal]').click();
-        cy.get('[data-cy=order-number]').should('not.exist');
+        cy.get(`[data-cy=${closeModal}]`).click();
+        cy.get(`[data-cy=${orderNumber}]`).should('not.exist');
         cy.contains('76767').should('not.exist');
     });
 
     // Проверяется, что конструктор пуст
     cy.get('[data-cy="bun"]').should('not.exist');
-    cy.get('[data-cy="switching"]').should('not.exist');
+    cy.get(`[data-cy=${switching}]`).should('not.exist');
   });
 })
